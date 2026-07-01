377a3e9436d905fd77f2d357ab0250528157ebec
LiliRd <li_rahmani@yahoo.com>
2026-07-01 10:26:39 +0330

    feat:ADD JACOCO

---
+++ src/main/java/com/example/AppController.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/hello/{name}")
+    public ResponseEntity<Double> getCurrentTemperature(@PathVariable String name) {
+        try {
+            System.out.println("Hello "+ name);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
